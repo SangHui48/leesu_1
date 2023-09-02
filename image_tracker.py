51fe36833cd9873dc19f0646f56fa5c33ef232cd
SangHui48 <vodlrm123@naver.com>
2023-09-03 01:06:48 +0900

    setting 완료

--- image_tracker.py
+++ image_tracker.py
@@ -1,10 +1,19 @@
-from selenium import webdriver
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.chrome.options import Options 
+
+ 
+
+ 
 
 def main():
-    driver = webdriver.Chrome()
+    # 브라우저 꺼짐 방지 옵션 
+    chrome_options = Options() 
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    driver = webdriver.Chrome(options=chrome_options)
     url = 'https://www.google.com'
     driver.get(url)
-
 
 
 if __name__ == "__main__":
